4398f56749b601b607b9e15c74b5d40cf558ae7d
nashmuhandes <nashbackslash@gmail.com>
2017-03-27 17:19:51 +0800

    Change default fog mode to Radial.

--- src/gl/renderer/gl_lightdata.cpp
+++ src/gl/renderer/gl_lightdata.cpp
@@ -91,7 +91,7 @@ CUSTOM_CVAR (Int, gl_distfog, 70, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
 	}
 }
 
-CUSTOM_CVAR(Int,gl_fogmode,1,CVAR_ARCHIVE|CVAR_NOINITCALL)
+CUSTOM_CVAR(Int,gl_fogmode,2,CVAR_ARCHIVE|CVAR_NOINITCALL)
 {
 	if (self>2) self=2;
 	if (self<0) self=0;
