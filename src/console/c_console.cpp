db8f4ec514209bcbc5d8ed935f2dd4c408c909d7
nashmuhandes <nashbackslash@gmail.com>
2019-08-20 05:54:03 +0800

    Revert "Increased notify time and default to center notifications."
    This reverts commit 29f57ab988db9884543f17cecab960241a132b93.
    
    # Conflicts:
    #	src/c_console.cpp

--- src/console/c_console.cpp
+++ src/console/c_console.cpp
@@ -132,9 +132,8 @@ EXTERN_CVAR (Bool, show_messages)
 static char *work = NULL;
 static int worklen = 0;
 
-// [LAD] increased notify time and default to center notifications
-CVAR(Float, con_notifytime, 10.f, CVAR_ARCHIVE)
-CVAR(Bool, con_centernotify, true, CVAR_ARCHIVE)
+CVAR(Float, con_notifytime, 3.f, CVAR_ARCHIVE)
+CVAR(Bool, con_centernotify, false, CVAR_ARCHIVE)
 CUSTOM_CVAR(Int, con_scaletext, 0, CVAR_ARCHIVE)		// Scale notify text at high resolutions?
 {
 	if (self < 0) self = 0;
